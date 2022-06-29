2bb0d66c2ad77a51f2e1ed1686b56ad7d7dff72d
Horider <Horider@yandex.ru>
2022-06-29 14:03:24 +0300

    Изменил связи бинов Egg6

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,10 +5,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-
+    @Autowired
     private final Needle7 needle7;
 
-    @Autowired
     public Egg6(Needle7 needle7) {
         this.needle7 = needle7;
     }
